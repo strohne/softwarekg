9ea63fb4af9948d90943d1b9d099e83b164c5809
strohne <krimskram@freenet.de>
2021-12-09 11:35:19 +0100

    Distinct softwares, group_infomap

--- 02_preparenetwork.R
+++ 02_preparenetwork.R
@@ -37,7 +37,10 @@ kg_software <- kg_software %>%
   mutate(
     softwaretype = str_remove(softwaretype,"<http://data.gesis.org/softwarekg/vocab/SoftwareType_"),
     softwaretype = str_remove(softwaretype,">$")
-  )
+  ) %>% 
+  group_by(sw) %>% 
+  slice_sample(n=1) %>% 
+  ungroup()
 
 # Mentions
 kg_mentions <- kg_mentions %>% 
@@ -61,15 +64,11 @@ kg_mentions <- kg_mentions %>%
   left_join(kg_concepts,by="journal") 
 
 
-#group_edge_betweenness
-#infomap.community()
-
-
 # Edges-List: Co-mentions
 com_edges <- kg_mentions %>% 
   select(item=article, feature=software) %>% 
   get_cooccurrence() %>% 
-  filter(n > 0) %>% 
+  filter(n > 0, source != target) %>% 
   mutate(weight=p_cond)
 
 
@@ -98,16 +97,17 @@ com_nodes <- kg_mentions %>%
 gr_com <- tbl_graph(com_nodes, com_edges, directed = T)
 
 
-# Add measures to graph
-# - detect communities (Louvain-Algorithm)
-# - compute centrality measures
+# Group detection alternatives for undirected graphs
+# group_edge_betweenness
+# infomap.community()
+# leiden
+
+#https://www.r-bloggers.com/2012/06/summary-of-community-detection-algorithms-in-igraph-0-6/
+
+
 gr_com <- gr_com %>% 
-  morph(to_undirected) %>%
   mutate(
-    community_no = as.factor(group_louvain(weights=p_cond))
-  ) %>% 
-  unmorph() %>% 
-  mutate(
+    community_no = as.factor(group_infomap(weights=p_cond)),
     graph_degree_in = centrality_degree(mode="in", weights=p_cond),
     graph_degree_out = centrality_degree(mode="out", weights=p_cond),
     graph_betweenness = centrality_betweenness(directed=T , weights=p_cond)
@@ -121,20 +121,20 @@ gr_com <- gr_com %>%
 com_communities <- gr_com %>% 
   as_tibble() 
 
-# TODO: count article types, pivot wider
+# TODO: count software types, pivot wider
 
 communities_data <-  gr_com %>%  
   morph(to_split, community_no) %>% 
   crystallise() %>%  
   mutate(
     density = map(graph,graph.density), 
-    clique_no = map(graph, count_max_cliques),
+    #clique_no = map(graph, count_max_cliques),
     n_edges = map(graph, gsize), 
     n_nodes = map(graph, gorder),
-    recpro = map(graph, reciprocity),
+    #recpro = map(graph, reciprocity),
     community_no = as.factor(row_number()),
-    community_degree = map(graph, degree),
-    community_betweenness = map(graph, betweenness)
+    #community_degree = map(graph, degree),
+    #community_betweenness = map(graph, betweenness)
   )
 
 com_nodes <-com_communities %>% 
